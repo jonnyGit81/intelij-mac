5b306028460544243a5dcf7457f1271acd9ba5f2
jonny <jonny.marcello@gmail.com>
2020-03-05 12:19:57 +0700

    initial commit

--- src/com/company/BlaBla.java
+++ src/com/company/BlaBla.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.io.*;
+import java.lang.annotation.*;
+
 public class BlaBla {
+    private String s1;
+    private String s2;
+    private String s3;
+
+    public void abc() throws IOException {
+        String json = "{\"name\": \"jonny\"}";
+        String exp = "/[a-z][0-9].$/";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("abc.txt"));
+
+        String line = bufferedReader.readLine();
+        s1
+    }
 }
+
+
